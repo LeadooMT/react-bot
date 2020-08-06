c6bf48f12e02b7e5168201f447b8f21ce3507ded
Perry Mitchell <perry@perrymitchell.net>
2020-08-06 08:47:13 +0300

    Add media partner stories

---
+++ stories/InPageBotMP.stories.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { InPageBot } from "../source/index";
+
+const DEMO_MEDIA = "TestMediaCode";
+const DEMO_MEDIA_BOT_CODE = "react-bot";
+
+export default { title: "InPageBot (Media Partner)" };
+
+export const successfulLookup = () => (
+    <InPageBot code={DEMO_MEDIA_BOT_CODE} mediaPartner={DEMO_MEDIA} seamless />
+);
+
+export const noBotForCode = () => (
+    <InPageBot code="example-empty-code" mediaPartner={DEMO_MEDIA} seamless />
+);
